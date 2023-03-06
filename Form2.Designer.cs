cb84f969771e00c85a9fff4002c798a0d4346414
aniam <aniam@LAPTOP-OTPQTNLN>
2023-03-06 17:56:46 +0100

    praca w domu

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.TimerLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -41,6 +41,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "START";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -50,22 +51,23 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "STOP";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label1
+            // TimerLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(319, 147);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 25);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "00:00:00";
+            this.TimerLabel.AutoSize = true;
+            this.TimerLabel.Location = new System.Drawing.Point(319, 147);
+            this.TimerLabel.Name = "TimerLabel";
+            this.TimerLabel.Size = new System.Drawing.Size(80, 25);
+            this.TimerLabel.TabIndex = 2;
+            this.TimerLabel.Text = "00:00:00";
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TimerLabel);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form2";
@@ -79,6 +81,6 @@
 
         private Button button1;
         private Button button2;
-        private Label label1;
+        private Label TimerLabel;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -15,6 +15,33 @@ namespace Wizulane_Laby_1
         public Form2()
         {
             InitializeComponent();
+            InitTimer();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startTime = DateTime.Now;
+            timer1.Start();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+        System.Windows.Forms.Timer timer1;
+        DateTime startTime;
+        public void InitTimer()
+        {
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(Timer1_Tick);
+            timer1.Interval = 100; // in miliseconds
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan duration = DateTime.Now - startTime;
+            DateTime time = DateTime.MinValue.Add(duration);
+            TimerLabel.Text = duration.ToString(@"dd\.hh\:mm\:ss\:ff");
+        }
+
     }
 }
